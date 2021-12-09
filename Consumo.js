47f8b945e766430e8d8188c9677fbaed05062364
Gabriel Cortés Garcia <tetradog@gmail.com>
2021-12-09 09:22:28 +0100

    Init
    de momento lo pongo junto

---
+++ Consumo.js
@@ -0,0 +1,79 @@
+//GET /{lang}/datos/{category}/{widget}?[query]
+//apidatos.ree.es
+
+class GeoLimit{
+
+  const Peninsular='peninsular';
+  const ComunidadAutonoma='ccaa';
+  const Canarias='canarias';
+  const Baleares='baleares';
+  const Ceuta='ceuta';
+  const Melilla='melilla';
+
+}
+class Categoria{
+  const Balance='balance';
+  const Demanada='demanda';
+  const Generacion='generacion';
+  const Intercambios='intercambios';
+  const Transporte='transporte';
+  const Mercados='mercados';
+}
+
+class Idioma{
+  const Spanish='es';
+  const English='en';
+
+}
+
+class GeoId{
+
+  
+  const Peninsular =	8741;
+  const Canarias =	8742;
+  const Baleares 	8743;
+
+  const Ceuta 	=8744;
+
+  const Melilla =	8745;
+
+  const Andalucia= 4;
+
+  const Aragon= 5;
+
+  const Cantabria= 6;
+
+  const CastillaLaMancha= 7;
+
+  const CastillaYLeon= 8;
+
+  const Catalunya= 9;
+
+  const PaisVasco= 10;
+
+  const PrincipadoDeAsturias= 11;
+
+  const ComunidadDeCeuta= 8744;
+
+  const ComunidadDeMelilla= 8745;
+
+  const ComunidadDeMadrid= 13;
+
+  const ComunidadDeNavarra= 14;
+
+  const ComunidadValenciana= 15;
+
+  const Extremadura= 16;
+
+  const Galicia= 17;
+
+  const IslasBaleares= 8743;
+
+  const IslasCanarias= 8742;
+
+  const LaRioja= 20;
+
+  const RegionDeMurcia= 21;
+
+
+}
